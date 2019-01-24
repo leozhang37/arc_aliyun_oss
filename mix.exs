f63c2d133a0efc498dbad6e928fa5a1d36cbc415
Zhang ZhiJiang <leozhang37@gmail.com>
2019-01-24 17:27:03 +0800

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -24,9 +24,9 @@ defmodule ArcAliyunOss.MixProject do
 
   defp deps do
     [
-      {:alixir_oss, github: "leozhang37/alixir_oss", branch: "develop"},
+      {:alixir_oss, github: "https://github.com/leozhang37/alixir_oss.git", branch: "develop"},
       {:arc, "~> 0.10.0"},
-      {:ex_arc, "~> 0.0.1", github: "leozhang37/ex_arc", branch: "develop"},
+      {:ex_arc, "~> 0.0.1", github: "https://github.com/leozhang37/ex_arc.git", branch: "develop"},
       {:httpoison, "~> 1.2.0", override: true}
     ]
   end
